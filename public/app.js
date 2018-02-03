4937fa9be4237369445af3166bab865b92eb225b
Noah Hunter <nhhntr11@gmail.com>
2018-02-03 02:51:56 +0000

    Updating infrastructure

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // Create a Feathers application
 const app = feathers();
 // Initialize a REST connection
-const rest = feathers.rest('http://localhost:3030');
+const rest = feathers.rest('http://wallet.ducatus.io:3030');
 // Configure the REST client by using `window.fetch`
 app.configure(rest.fetch(window.fetch));
 
@@ -10,6 +10,7 @@ app.service('api_key').on('created', message => {
 });
 
 async function createAndList() {
+    console.log("HELLO");
   await app.service('api_key').create({
     text: 'Hello from Feathers browser client'
   });
